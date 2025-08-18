2f4e0fc9054594f57a198965b2450f765584c85d
うたうたげ <kotalegoblock@gmail.com>
2025-08-19 07:26:43 +0900

    データの保持方法を更新

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 「[Hack!Hokuriku　北陸観光をハックせよ！ハッカソン](https://connpass.com/event/334998/)」で考案したシステム
 
-以下のファイルは[hokuriku-inbound-kanko/m5](https://github.com/hokuriku-inbound-kanko/m5)から転用：
-- `00_M5_facility list.csv`
-- `0924_1101.csv`
-- `1024_1209.csv`
+以下のファイルは[`hokuriku-inbound-kanko/m5`](https://github.com/hokuriku-inbound-kanko/m5)に公開されているオープンデータを一部改変して使用：
+- `facilities.json`
+- `stacks.csv`
--- main.js
+++ main.js
@@ -1,38 +1,38 @@
 test();
 
 async function test() {
-    const response = await fetch("https://raw.githubusercontent.com/hokuriku-inbound-kanko/mac-address/refs/heads/main/00_M5_facility%20list.csv");
-    const text = await response.text();
-    let lines = text.split("\n");
-    lines = lines.filter((line) => !/^\,*$/.test(line));
-    for (let i = 0; i < lines.length; i++) {
-        lines[i] = lines[i].split(",");
-    }    
-    const table = document.createElement("table");
-    const thead = document.createElement("thead");
-    const tbody = document.createElement("tbody");
+    // const response = await fetch("https://raw.githubusercontent.com/hokuriku-inbound-kanko/mac-address/refs/heads/main/00_M5_facility%20list.csv");
+    // const text = await response.text();
+    // let lines = text.split("\n");
+    // lines = lines.filter((line) => !/^\,*$/.test(line));
+    // for (let i = 0; i < lines.length; i++) {
+    //     lines[i] = lines[i].split(",");
+    // }    
+    // const table = document.createElement("table");
+    // const thead = document.createElement("thead");
+    // const tbody = document.createElement("tbody");
     
-    // ヘッダー行を生成
-    const headerRow = document.createElement("tr");
-    Object.keys(lines[0]).forEach((key) => {
-        const th = document.createElement("th");
-        th.textContent = key;
-        headerRow.appendChild(th);
-    });
-    thead.appendChild(headerRow);
+    // // ヘッダー行を生成
+    // const headerRow = document.createElement("tr");
+    // Object.keys(lines[0]).forEach((key) => {
+    //     const th = document.createElement("th");
+    //     th.textContent = key;
+    //     headerRow.appendChild(th);
+    // });
+    // thead.appendChild(headerRow);
     
-    // データ行を生成
-    lines.forEach((row) => {
-        const tr = document.createElement("tr");
-        Object.values(row).forEach((value) => {
-            const td = document.createElement("td");
-            td.textContent = value;
-            tr.appendChild(td);
-        });
-        tbody.appendChild(tr);
-    });
+    // // データ行を生成
+    // lines.forEach((row) => {
+    //     const tr = document.createElement("tr");
+    //     Object.values(row).forEach((value) => {
+    //         const td = document.createElement("td");
+    //         td.textContent = value;
+    //         tr.appendChild(td);
+    //     });
+    //     tbody.appendChild(tr);
+    // });
     
-    table.appendChild(thead);
-    table.appendChild(tbody);
-    document.getElementById("output").appendChild(table);
+    // table.appendChild(thead);
+    // table.appendChild(tbody);
+    // document.getElementById("output").appendChild(table);
 }
